0120a345871d46d213a16a4c58f60d7d1af67fed
y.alsayed <y.alsayed@nu.edu.eg>
2024-03-24 10:35:41 +0200

    update build script to deploy locally

--- build.bash
+++ build.bash
@@ -1,8 +1,10 @@
 cd /home/yusuf/Documents/lebaladna_app/frontend
 flutter clean
 flutter pub upgrade --major-versions
-flutter build web --web-renderer canvaskit --release
+flutter build web --web-renderer canvaskit --no-web-resources-cdn --release
 cd build/web
+sudo rm -r /usr/share/nginx/test/*
+sudo cp -r * /usr/share/nginx/test/
 zip web -r *
 mv web.zip ../../../
 
