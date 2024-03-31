26895ad2cddcd53fccb1eaf6c5a3c6dd1a5bc134
y.alsayed <y.alsayed@nu.edu.eg>
2024-03-31 21:58:52 +0200

    edit deploy script to deploy to remote server

--- build_deploy.bash
+++ build_deploy.bash
@@ -1,10 +1,21 @@
-cd /home/yusuf/Documents/lebaladna_app/frontend # Use an absolute path (Use your own path) 
+# Use an absolute path (Use your own path) 
+# Frontend build + deployment:
+cd /home/yusuf/Documents/lebaladna_app/frontend
 flutter clean
 flutter pub upgrade --major-versions
-flutter build web --web-renderer canvaskit --no-web-resources-cdn --release
+flutter build web --web-renderer canvaskit --release
 cd build/web
-sudo rm -r /usr/share/nginx/test/*
-sudo cp -r * /usr/share/nginx/test/
-zip web -r *
-mv web.zip ../../../
+scp -i /home/yusuf/Desktop/Server/sshKeys/backend -r * ubuntu@204.216.221.89:/home/ubuntu/lebaladna/frontend 
+ssh  ubuntu@204.216.221.89 -i /home/yusuf/Desktop/Server/sshKeys/backend "cd /home/ubuntu/lebaladna/frontend; sudo cp -r * /usr/share/nginx/lebaladna/; sudo systemctl restart nginx"
 
+# Local deployment:
+# sudo rm -r /usr/share/nginx/test/*
+# sudo cp -r * /usr/share/nginx/test/
+# zip web -r *
+# mv web.zip ../../../
+
+# Backend build + deployment:
+cd /home/yusuf/Documents/lebaladna_app/backend
+npx tsc
+scp -i /home/yusuf/Desktop/Server/sshKeys/backend -r .env package-lock.json package.json ./build/ ubuntu@204.216.221.89:/home/ubuntu/lebaladna/backend 
+ssh  ubuntu@204.216.221.89 -i /home/yusuf/Desktop/Server/sshKeys/backend "cd /home/ubuntu/lebaladna/backend; source ~/.nvm/nvm.sh; npm i; pm2 restart lebaladna"
